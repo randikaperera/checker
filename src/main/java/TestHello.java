27764af65a7fa4c4b279cf662760059e94711ba1
RPerera <perera.randika@gmail.com>
2019-03-06 21:27:23 +0530

    second commit

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,6 +1,6 @@
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("This is the test program123");
+        System.out.println("This is the test program12345");
         System.out.println("new line 12");
         System.out.println("Hello people");
         System.out.println("hello students");
