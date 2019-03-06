a01acc47ac3a800bbbffffb9dbcb053354bfbf74
RPerera <perera.randika@gmail.com>
2019-03-06 21:27:23 +0530

    second commit

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,8 +1,5 @@
 public class TestHello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("This is the test program123");
-        System.out.println("new line 122222222222");
         System.out.println("Hello people");
         System.out.println("hello students");
         System.out.println("hi this line is changed by randika perera");
